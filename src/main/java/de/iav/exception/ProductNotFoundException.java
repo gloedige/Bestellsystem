30caf02640bf148c7d9dcffb971cdbff56c1476d
Gerd Lödige <gerd.loedige@posteo.de>
2023-05-24 12:20:59 +0200

    Create ProductNotFoundException

---
+++ src/main/java/de/iav/exception/ProductNotFoundException.java
@@ -0,0 +1,9 @@
+package de.iav.exception;
+
+import java.util.NoSuchElementException;
+
+public class ProductNotFoundException extends NoSuchElementException {
+    public ProductNotFoundException(String id) {
+        super("Produkt mit der ID " +id+ " konnte nicht gefunden werden");
+    }
+}
